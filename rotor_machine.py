127213651cf30f57658cac0ccc4f8c28fd1fc2ed
David <d4v1d1431@gmail.com>
2024-11-03 23:58:08 -0400

    Last changes

--- rotor_machine.py
+++ rotor_machine.py
@@ -1,7 +1,6 @@
 import string, random
 from numpy import *
 from collections import deque
-
 #filling alphabet uppercase in the array
 alphabet = []
 alphabet = list(string.ascii_uppercase)
@@ -15,9 +14,7 @@ random.shuffle(secondconfig)
 thirdconfig = alphabet.copy()
 random.shuffle(thirdconfig)
 input = ['HOPE','HELLO','NEW YEAR']
-c1=0
-c2=0
-c3=0
+c1 = c2= c3 = 0
 output=""
 for caracter in input[0]:
     if caracter.isalpha():
@@ -38,6 +35,27 @@ for caracter in input[0]:
     else:
         output+=caracter
 print ("Encrypted text : " + output)
+print ("List after insertion : " + str(alphabet))
 print ("List after insertion : " + str(firstconfig))
 print ("List after insertion : " + str(secondconfig))
 print ("List after insertion : " + str(thirdconfig))
+
+c1 = c2= c3 = 0
+for caracter in output:
+    if caracter.isalpha():
+        r3 = alphabet[thirdconfig.index(caracter)-c3]
+        r2 = alphabet[secondconfig.index(r3)-c2]
+        r1 = alphabet[firstconfig.index(r2)-c1]
+        output+=r1
+        print(caracter+"=>"+r3+"=>"+r2+"=>"+r1)
+        c1+=1
+        if c1==26:
+            c1=0
+            c2+=1
+        if c2==26:
+            c2=0
+            c3+=1
+        if c3==26:
+            c3=0
+    else:
+        output+=caracter
